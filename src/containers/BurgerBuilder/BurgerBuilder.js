2990d7c8630d64ec14adfe2d58aa33a8ff6e47c9
Kevin Roberts-Godinho <39806291+KevinGodinho@users.noreply.github.com>
2019-06-02 20:59:28 -0700

    New Features
    The app now has a burger menu icon that displays a side bar when selected. The side bar menu is removed when the backdrop is clicked. When the sidebar is brought out, the backdrop is darkened. The order menu populates the total price along with buttons to order or cancel the order. Cancelling makes the order menu go away, so you can go back to building your burger.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,6 +80,14 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({purchasing: true});
     }
+    
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+    
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
 
     render() {
         const disabledInfo = {
@@ -90,8 +98,12 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-              <Modal show={this.state.purchasing}>
-                <OrderSummary ingredients={this.state.ingredients}/>
+              <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                <OrderSummary 
+                  ingredients={this.state.ingredients}
+                  price={this.state.totalPrice}
+                  purchaseCancelled={this.purchaseCancelHandler}
+                  purchaseContinued={this.purchaseContinueHandler}/>
               </Modal>
               <Burger ingredients={this.state.ingredients} />
               <BuildControls 
